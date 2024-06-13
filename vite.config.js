238c00c0ff6f0f39f32a31956ece8e8e416329ee
mcnoble1 <idowufestustemiloluwa@gmail.com>
2024-06-13 16:11:15 +0100

    feat: updates :octocat:

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
-      'process.env.REACT_APP_GOOGLE_API_KEY': JSON.stringify(env.REACT_APP_GOOGLE_API_KEY)
+      'process.env.REACT_APP_GOOGLE_API_KEY': JSON.stringify(env.REACT_APP_GOOGLE_API_KEY),
+      'process.env.CONVEX_DEPLOYMENT': JSON.stringify(env.CONVEX_DEPLOYMENT),
+      'process.env.VITE_CONVEX_URL': JSON.stringify(env.VITE_CONVEX_URL),
     },
     plugins: [react(), viteCommonjs()],
     build: {
