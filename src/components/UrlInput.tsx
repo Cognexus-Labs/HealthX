d9bf9e72a87e07f6e9bb01ff0c365f1df1ad1fa1
mcnoble1 <idowufestustemiloluwa@gmail.com>
2024-06-15 17:53:24 +0100

    feat: updated url component :octocat:

--- src/components/UrlInput.tsx
+++ src/components/UrlInput.tsx
@@ -1,16 +1,27 @@
-// src/components/UrlInput.jsx
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
-const UrlInput = ({ addSummary }) => {
+const UrlInput = () => {
   const [url, setUrl] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [summary, setSummary] = useState('');
 
   const handleSubmit = async () => {
+    if (!url) {
+      toast.error('No URL entered');
+      return;
+    }
+    setIsLoading(true);
     try {
       const response = await axios.post('https://summarizeanything.azurewebsites.net/youtube2summary', { youtube_link: url });
-      addSummary(response.data.summary);
+      setSummary(response.data.summary);
+      setUrl('');
     } catch (error) {
       console.error('Error summarizing URL:', error);
+        toast.error('Invalid URL, try again');
+    } finally {
+        setIsLoading(false);
     }
   };
 
@@ -27,9 +38,32 @@ const UrlInput = ({ addSummary }) => {
       <button
         className="mt-4 bg-blue-500 text-white bg-primary px-4 py-2 rounded"
         onClick={handleSubmit}
+        disabled={isLoading}
       >
-        Summarize
+        {isLoading ? (
+            <div className="flex items-center">
+                <div className="spinner"></div>
+                <span className="pl-1">Summarizing...</span>
+            </div>
+            ) : (
+            <>Summarize</>
+            )}
       </button>
+
+      {summary && (
+        <div className="mt-4 p-4 bg-gray-100 rounded">
+            <div className='w-full flex flex-row justify-between'>
+            <h3 className="text-lg font-bold mb-2">Youtube Summary</h3>
+            <button
+                className="bg-primary text-white px-4 py-1 rounded"
+                onClick={() => setSummary('')}
+            >
+                Clear
+            </button>
+            </div>
+            <p>{summary}</p>
+        </div>
+        )}
     </div>
   );
 };
